97c2275d56e42a9a7b89b8ec60c1181b90bb2aca
NickZhang <maccus0329@gmail.com>
2021-12-21 00:50:52 +0800

    deploy

---
+++ precache-manifest.0cc71b8c79c7663f8c7bdcf5d577f48e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f26072b56e46c10b26bf",
+    "url": "/costsav-website/css/app.70aaf655.css"
+  },
+  {
+    "revision": "bb32d9cb6ab09e152062be3e84ef638f",
+    "url": "/costsav-website/index.html"
+  },
+  {
+    "revision": "f26072b56e46c10b26bf",
+    "url": "/costsav-website/js/app.70b4033f.js"
+  },
+  {
+    "revision": "ef7073e0fdde787dc1c6",
+    "url": "/costsav-website/js/chunk-vendors.25802b2a.js"
+  },
+  {
+    "revision": "d5da4d5596f740e381bef955d359d063",
+    "url": "/costsav-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/costsav-website/robots.txt"
+  }
+]);
